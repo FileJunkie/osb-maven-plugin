b3477ed4bb0a114f3d9300198b322129f40dde18
Ilya "FileJunkie" Ershov <filejunkie@gmail.com>
2014-05-11 15:02:18 +0400

    "Hello, world" state

---
+++ src/main/scala/name/filejunkie/MainMojo.scala
@@ -0,0 +1,13 @@
+package name.filejunkie
+
+import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugin.AbstractMojo
+
+
+@Mojo( name = "generate")
+class MainMojo extends AbstractMojo {
+
+  override def execute(): Unit = {
+    getLog().info("Hello, world.")
+  }
+}
