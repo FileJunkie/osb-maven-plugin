783c0f65d1ddaf4aa192ad2bf9c52bf0e2d77243
Ilya Ershov <filejunkie@gmail.com>
2014-05-13 09:10:33 +0400

    Another unit test

--- src/test/scala/name/filejunkie/osbmavenplugin/manager/ExportInfoTest.scala
+++ src/test/scala/name/filejunkie/osbmavenplugin/manager/ExportInfoTest.scala
@@ -4,6 +4,7 @@ import java.io.File
 import org.specs2.mutable._
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
+import name.filejunkie.osbmavenplugin.osbentities.OSBEntity
 
 @RunWith(classOf[JUnitRunner])
 object ExportInfoTest extends Specification {
@@ -12,6 +13,18 @@ object ExportInfoTest extends Specification {
       val exportInfo = new ExportInfo(new File("dummy"))
 
       exportInfo.exportInfoText.replaceAll("\\s","") must be matching ".*<\\/imp:properties><\\/xml-fragment>.*"
+    }
+
+    "have entities lines written" in {
+      val exportInfo = new ExportInfo(new File ("dummy"))
+      val osbEntity = new OSBEntity {
+        override def writeFile: Unit = {}
+        override def exportInfoLine: String = "dummyOSBEntity"
+      }
+
+      exportInfo.addOSBEntity(osbEntity)
+
+      exportInfo.exportInfoText.replaceAll("\\s","") must be matching ".*<\\/imp:properties>dummyOSBEntity<\\/xml-fragment>.*"
     }
   }
 }
